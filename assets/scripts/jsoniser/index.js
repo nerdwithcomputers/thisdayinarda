879b7dd5ab4d68d8c94365bf60dd519bd0b30665
nerdwithcomputers <hpscigeek@gmail.com>
2025-05-16 23:16:06 -0400

    json sucks and i hate it

--- assets/scripts/jsoniser/index.js
+++ assets/scripts/jsoniser/index.js
@@ -1,25 +1,35 @@
 const fs = require('node:fs');
 const header = /(\d)+_/
-var final = new Map();
+var map = new Map();
 
-fs.readFile('../tolkien.txt', 'utf-8', (err,data)=>{
+fs.readFile('../../tolkien.txt', 'utf-8', (err,data)=>{
     if(err){
         console.error(err);
         return;
     }
-    // console.log(data)
-    return data;
-}).then((data)=>{
+    // console.log(data);
     var splitArr = data.split('\n');
-    // var capturing = false;
+    splitArr = splitArr.filter((word)=> word.length>0);
+    console.log(splitArr);
     var date = "";
     for(const el of splitArr){
         if(el.match(header)){
             date = el;
+            map.set(date,);
             continue;
         }else{
-            final.set(date, el);
+            let arr = map.get(date);
+            arr.append(el);
+            map.set(date,arr);
         }
     }
-    
+    json = Object.fromEntries(map);
+    final = JSON.stringify(json);
+    fs.writeFile('tolkien.json',final,(err,data)=>{
+        if(err){
+            console.error(err);
+        }else{
+            console.log('SUCCESS!!!');
+        }
+    });
 })
