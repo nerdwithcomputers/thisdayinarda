519da568911896887ad98fbd4442fa8ee9a394bc
nerdwithcomputers <hpscigeek@gmail.com>
2025-05-25 13:08:06 -0400

    ack

--- app/index.jsx
+++ app/index.jsx
@@ -1,18 +1,15 @@
 import { useEffect, useState } from "react";
 import { Text, View, ScrollView, useWindowDimensions } from "react-native";
-// import RenderHTML from "react-native-render-html";
-// import DOMParser from 'react-native-html-parser';
-import cheerio from "react-native-cheerio";
-
-// line ~400 of source is where i want
+const calendar = require('./assets/tolkien.json');
 
 function Retrieve(){
-  const [get, setGet] = useState(['loading...']);
+  const [get, setGet] = useState('loading');
   const date = Date();
   var month = '';
   var day = '';
   // console.log(date);
   const dateArr=date.split(' ');
+  day = dateArr[0];
   switch(dateArr[1]){
     case'Jan': month='January'; break;
     case'Feb': month='February'; break;
@@ -27,34 +24,8 @@ function Retrieve(){
     case'Oct': month='October'; break;
     case'Dec': month='December'; break;
   }
-  console.log(month)
-  if(dateArr[2][0]=='0') day = dateArr[2][1];
-  else day = dateArr[2];
-  useEffect(()=>{
-    console.log(`https://tolkiengateway.net/wiki/${day}_${month}`);
-    fetch('http://m1raclemax.hackclub.app:42449', 
-      {
-        headers: {
-          "url":`https://tolkiengateway.net/wiki/${day}_${month}`
-        }
-      }
-    ).then(
-      (x)=>{
-        return x.text();
-      }
-    ).then(
-      (x)=>{
-        console.log('hello');
-        console.log(x);
-        const $ = cheerio.load(x);
-        let text = $('#citizen-section-2').text();
-        console.log(text);
-        setGet(text);
-        // console.log(x);
-      }
-    );
-    // console.log('end of line');
-  });
+  console.log(month);
+  setGet(calendar[`${day}_${month}`]);
   return(
     <>
       <Text
