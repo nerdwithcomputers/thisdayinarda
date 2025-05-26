6efd28832a1535b455b83dca5ea5c3ee98568604
nerdwithcomputers <hpscigeek@gmail.com>
2025-05-26 15:22:23 -0400

    added stylesheet file

--- app/index.jsx
+++ app/index.jsx
@@ -1,52 +1,10 @@
-import { useEffect, useState } from "react";
-import {StyleSheet, Text, View, ScrollView, useWindowDimensions} from "react-native";
+import { useState } from "react";
+import {Text, View, ScrollView, useWindowDimensions} from "react-native";
+import sheet from "./style";
 const calendarArda = require('./../assets/tolkien.json');
 const calendarIRL = require('./../assets/irl.json');
-const palette = [
-  '#bd1919',
-  '#229c9c',
-  '#19bdbd',
-  '#008585',
-  '#004343',
-  '#002222'
-];
-const palette1 = [
-  '#a2c8a7',
-  '#6aa96f',
-  '#3a7e44',
-  '#1a6033',
-  '#1a6033',
-  '#0b3c20'
-]
-const sheet = StyleSheet.create({
-  container:{
-    flex: 3,
-    // margin:10,
-    backgroundColor: palette[5]
-  },
-  title:{
-    color: palette[5],
-    textAlign:"center",
-    fontSize:60,
-    // flex:1,
-    backgroundColor:palette[3],
-    marginTop:30
-  },
-  dateCardText:{
-    color:palette[0],
-    align:"center",
-    textAlign:"left",
-    flex:1,
-    margin:15,
-    width:'75%',
-  },
-  dateCardBox:{
-    alignItems:"center",
-    backgroundColor:palette[4],
-    borderRadius: 30,
-    marginTop:30
-  }
-})
+
+// console.log(sheet);
 
 function Retrieve(calendar){
   const date = Date();
@@ -110,6 +68,9 @@ export default function Index() {
       <ScrollView>
         <Text style={sheet.title} onPress={swap}>
           Today in {which}
+        </Text>
+        <Text style={sheet.subtitle}>
+          click to swap from arda to irl
         </Text>
         <Retrieve which={calendar}/>
       </ScrollView>
---
+++ app/style.jsx
@@ -0,0 +1,63 @@
+import { StyleSheet } from "react-native";
+
+// const {height, width} = useWindowDimensions();
+const palette = [
+  '#bd1919',
+  '#229c9c',
+  '#19bdbd',
+  '#008585',
+  '#004343',
+  '#002222'
+];
+const palette1 = [
+  '#a2c8a7',
+  '#6aa96f',
+  '#3a7e44',
+  '#1a6033',
+  '#1a6033',
+  '#0b3c20'
+]
+const sheet = StyleSheet.create({
+  container:{
+    flex: 3,
+    // margin:10,
+    backgroundColor: palette[5]
+  },
+  title:{
+    color: palette[5],
+    textAlign:"center",
+    fontSize:60,
+    // flex:1,
+    backgroundColor:palette[3],
+    marginTop:30,
+    // borderBottomRightRadius: 30
+  },
+  subtitle:{
+    color: palette[4],
+    textAlign:"center",
+    fontSize:20,
+    // flex:1,
+    backgroundColor:palette[3],
+    // marginTop:30,
+    borderBottomRightRadius: 30
+  },
+  dateCardText:{
+    color:palette[0],
+    fontSize:18,
+    align:"center",
+    textAlign:"left",
+    flex:1,
+    margin:15,
+    width:"80%",
+  },
+  dateCardBox:{
+    alignItems:"center",
+    backgroundColor:palette[4],
+    borderRadius: 30,
+    marginTop:30,
+    width:"80%",
+    alignSelf:"center"
+  }
+})
+
+export default sheet
